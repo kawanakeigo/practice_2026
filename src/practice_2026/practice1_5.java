e2a74ce3c37a7a7825d77fd760e45bc2d6915960
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 21:06:56 +0900

    比較演算

---
+++ src/practice_2026/practice1_5.java
@@ -0,0 +1,18 @@
+package practice_2026;
+
+public class practice1_5 {
+	public static void main(String[] args) {
+		int x=5;
+		int y=10;
+		int z=5;
+		
+		boolean a =(x >y);
+		boolean b =(y>z);
+		boolean c =(z>x);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+}
