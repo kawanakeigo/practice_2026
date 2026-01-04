0ab84e93ecf2b1d313af06238bc28887d6997db2
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 18:54:24 +0900

    変数の初期化をした

--- src/practice_2026/practice1_3.java
+++ src/practice_2026/practice1_3.java
@@ -2,11 +2,11 @@ package practice_2026;
 
 public class practice1_3 {
 	public static void main(String[] args) {
-		String name;
-		int age;
+		String name="Geekくん";
+		int age= 15;
 		
-		name ="Geekくん";
-		age = 15;
+		name ="Geekさん";
+		age = 25;
 		
 		System.out.println(name);
 		System.out.println(age);
