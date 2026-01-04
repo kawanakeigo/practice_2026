a3fd20887e2155052094a925c4ddc208bd29185b
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 21:27:17 +0900

    条件if文

---
+++ src/practice_2026/practice1_6.java
@@ -0,0 +1,23 @@
+package practice_2026;
+
+public class practice1_6 {
+	public static void main(String[] args) {
+		int number =20;
+		System.out.println("更新前");
+		if (number >15) {
+			System.out.println("15以上です");
+		}
+		
+		number = 14;
+		System.out.println("更新後");
+		if(number >=15) {
+			System.out .println("十五以上です");
+			
+		}else{
+			System.out.println("15以下です");
+		}
+		
+		System.out.println("完了");
+		
+	}
+}
