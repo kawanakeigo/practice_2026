e37be85696356c2441c2a339c93c3b40d315fff3
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 23:54:56 +0900

    if文の条件にbooleanの戻り値を返すメソッド

--- src/practice_2026/practice1_10.java
+++ src/practice_2026/practice1_10.java
@@ -9,9 +9,30 @@ public class practice1_10 {
 			num ++;
 		}
 		
+		int result =sumNumber(3,4);
+		System.out.println(result);
+		
+		int targetNumber = 2;
+		
+		if(isEven(targetNumber)) {
+			System.out.println("偶数である");
+		}else {
+			System.out.println("奇数である");
+		}
+		
 	}
 	
 	public static void hello(int num, String code) {
 		System.out.println("No."+ num + ": Hello," + code);
+	}
+	
+	public static int sumNumber(int a,int b) {
+		int sum = a+b;
+		return sum;
+		
+	}
+	
+	public static boolean isEven(int num) {
+		return num % 2==0;
 	}
 }
