4e01a4cb7355515e924747aa9992a83a90df2e14
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 23:35:52 +0900

    複数の引数を指定したメソッド

--- src/practice_2026/practice1_10.java
+++ src/practice_2026/practice1_10.java
@@ -2,10 +2,16 @@ package practice_2026;
 
 public class practice1_10 {
 	public static void main(String[] args) {
-		hello();
+		String[] countoryCode = {"JP","UK","USA"};
+		int num =1;
+		for (String code:countoryCode) {
+			hello(num,code);
+			num ++;
+		}
+		
 	}
 	
-	public static void hello() {
-		System.out.println("Hello World");
+	public static void hello(int num, String code) {
+		System.out.println("No."+ num + ": Hello," + code);
 	}
 }
