910e7ef7b244ff1f2796ef1751741c08c5be41d9
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 23:23:23 +0900

    メソッドを使ってhelloWorldを出す

---
+++ src/practice_2026/practice1_10.java
@@ -0,0 +1,11 @@
+package practice_2026;
+
+public class practice1_10 {
+	public static void main(String[] args) {
+		hello();
+	}
+	
+	public static void hello() {
+		System.out.println("Hello World");
+	}
+}
