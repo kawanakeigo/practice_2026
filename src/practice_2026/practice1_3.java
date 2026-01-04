9fc15eea71f2fdfc324c607fb81334d2f06d71d4
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 18:52:50 +0900

    mainに名前をしないとちゃんと実行できなかったのはなぜ？

--- src/practice_2026/practice1_3.java
+++ src/practice_2026/practice1_3.java
@@ -1,11 +1,14 @@
 package practice_2026;
 
 public class practice1_3 {
-	public static void def(String[] args) {
+	public static void main(String[] args) {
 		String name;
 		int age;
 		
 		name ="Geekくん";
 		age = 15;
+		
+		System.out.println(name);
+		System.out.println(age);
 	}
 }
