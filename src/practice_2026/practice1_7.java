538f21d76bfbdca7d30f74fdc97715aa98f14b2d
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 21:37:40 +0900

    繰り返しのwhile文

---
+++ src/practice_2026/practice1_7.java
@@ -0,0 +1,14 @@
+package practice_2026;
+
+public class practice1_7 {
+	public static void main(String[] args) {
+		int n =1;
+		
+		while(n <=100) {
+			System.out.println(n);
+			
+			n +=1;
+		}
+		
+	}
+}
