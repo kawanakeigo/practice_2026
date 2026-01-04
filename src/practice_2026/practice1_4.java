cc43c3cf6926eaac54e1f34495bed8b2c642844c
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-04 19:10:40 +0900

    キャストしてみた

---
+++ src/practice_2026/practice1_4.java
@@ -0,0 +1,13 @@
+package practice_2026;
+
+public class practice1_4 {
+	public static void main(String[] args) {
+		double a =3.0;
+		int b =4;
+		
+		//キャストなし
+		System.out.println(a * b);
+		//以下キャストあり
+		System.out.println((int)a*b);
+	}
+}
