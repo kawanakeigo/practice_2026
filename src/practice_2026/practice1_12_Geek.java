54d3cb6bc90a4bb91547b6f07eb55c64ae50b8d2
川名慶吾 <kawanakeiware@kawanakeiwarenoMacBook-Pro.local>
2026-01-08 04:41:39 +0900

    クラス

---
+++ src/practice_2026/practice1_12_Geek.java
@@ -0,0 +1,13 @@
+package practice_2026;
+
+public class practice1_12_Geek {
+	public String name;
+	public String color;
+	public double heigt;
+	
+	public void greeting() {
+		System.out.println("ヤッホ！"+ this.name + "です");
+	}
+	
+	
+}
