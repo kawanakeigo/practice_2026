4c44920a947c33b4221cfca3d0419a2f3a677a89
川名　慶吾 <k5@kawanakeiwarenoMac-mini.local>
2026-01-19 22:00:02 +0900

    hoge

---
+++ src/practice_2026/practice1_14_inherutance1.java
@@ -0,0 +1,10 @@
+package practice_2026;
+
+public class practice1_14_inherutance1 {
+	public static void main(String[] args) {
+		practice1_14_inheritance dog = new practice1_14_inheritance("しば犬", "芝犬",9.5);
+		dog.greeting();
+		
+		
+	}
+}
