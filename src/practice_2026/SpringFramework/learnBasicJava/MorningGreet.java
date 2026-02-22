bf96224b35613fdf828b03758c3b295f3c185958
川名　慶吾 <k5@kawanakeiwarenoMac-mini.local>
2026-02-22 23:13:08 +0900

    Java基礎ファイルを作成した。インターフェースを実装した

---
+++ src/practice_2026/SpringFramework/learnBasicJava/MorningGreet.java
@@ -0,0 +1,9 @@
+package practice_2026.SpringFramework.learnBasicJava;
+
+public class MorningGreet implements Greet{
+	@Override
+	public void greeting() {
+		System.out.println("おはようございます");
+		
+	}
+}
